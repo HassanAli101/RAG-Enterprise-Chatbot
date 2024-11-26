1c621cc49ff4887fe6e06a937798c7ee7d9afe74
Tariq Mehmood <tariq9mehmood9@gmail.com>
2024-11-26 22:05:18 +0500

    sql_agent: refactor into a class for better multi agent integrations in the commits to follow
    Signed-off-by: Tariq Mehmood <tariq9mehmood9@gmail.com>

--- sql_agent/src/agent/sql_agent.py
+++ sql_agent/src/agent/sql_agent.py
@@ -18,13 +18,13 @@ from langgraph.prebuilt import ToolNode
 from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
-db = SQLDatabase.from_uri(os.getenv("DATABASE_URL"))
+db = SQLDatabase.from_uri(os.getenv("DATABASE_URI_RO"))
 toolkit = SQLDatabaseToolkit(db=db, llm=model)
 tools = toolkit.get_tools()
 
 dialect = "PostgreSQL"
 top_k = 5
-instructions = """You are an agent designed to interact with a SQL database.
+system_message = """You are an agent designed to interact with a SQL database.
 Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
 Always limit your query to at most {top_k} results.
 You can order the results by a relevant column to return the most interesting examples in the database.
@@ -44,48 +44,61 @@ class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
     is_last_step: IsLastStep
 
-def wrap_model(model: BaseChatModel) -> RunnableSerializable[AgentState, AIMessage]:
-    model = model.bind_tools(tools)
-    preprocessor = RunnableLambda(
-        lambda state: [SystemMessage(content=instructions)] + state["messages"],
-        name="StateModifier",
-    )
-    return preprocessor | model
 
-async def acall_model(state: AgentState, config: RunnableConfig) -> AgentState:
-    model_runnable = wrap_model(model)
-    response = await model_runnable.ainvoke(state, config)
+class SqlAgent:
+    def __init__(self, model: BaseChatModel, db, tools):
+        self.model = model
+        self.db = db
+        self.tools = tools
+        self.agent = self._build_agent()
 
-    if state["is_last_step"] and response.tool_calls:
-        return {
-            "messages": [AIMessage(id=response.id, content="Sorry, need more steps to process this request.")]
-        }
-    # We return a list, because this will get added to the existing list
-    return {"messages": [response]}
+    def _wrap_model(self) -> RunnableSerializable[AgentState, AIMessage]:
+        self.model = self.model.bind_tools(self.tools)
+        preprocessor = RunnableLambda(
+            lambda state: [SystemMessage(content=system_message)] + state["messages"],
+            name="StateModifier",
+        )
+        return preprocessor | self.model
 
-# After "model", if there are tool calls, run "tools". Otherwise END.
-def pending_tool_calls(state: AgentState) -> Literal["tools", "done"]:
-    last_message = state["messages"][-1]
-    if not isinstance(last_message, AIMessage):
-        raise TypeError(f"Expected AIMessage, got {type(last_message)}")
-    if last_message.tool_calls:
-        return "tools"
-    return "done"
+    async def acall_model(self, state: AgentState, config: RunnableConfig) -> AgentState:
+        model_runnable = self._wrap_model()
+        response = await model_runnable.ainvoke(state, config)
+
+        if state["is_last_step"] and response.tool_calls:
+            return {
+                "messages": [AIMessage(id=response.id, content="Sorry, need more steps to process this request.")]
+            }
+        return {"messages": [response]}
+
+    def pending_tool_calls(self, state: AgentState) -> Literal["tools", "done"]:
+        last_message = state["messages"][-1]
+        if not isinstance(last_message, AIMessage):
+            raise TypeError(f"Expected AIMessage, got {type(last_message)}")
+        if last_message.tool_calls:
+            return "tools"
+        return "done"
+
+    def _build_agent(self) -> StateGraph:
+        # Define the graph
+        agent = StateGraph(AgentState)
+
+        agent.add_node("model", self.acall_model)
+        agent.add_node("tools", ToolNode(self.tools))
+
+        agent.add_edge(START, "model")
+        agent.add_edge("tools", "model")  # Always run "model" after "tools"
+
+        agent.add_conditional_edges(
+            "model", self.pending_tool_calls, {"tools": "tools", "done": END}
+        )
+
+        # Compile the agent with a checkpointer
+        return agent.compile(
+            checkpointer=MemorySaver(),
+        )
+
+    def get_agent(self):
+        return self.agent
 
 
-# Define the graph
-agent = StateGraph(AgentState)
-
-agent.add_node("model", acall_model)
-agent.add_node("tools", ToolNode(tools))
-
-agent.add_edge(START, "model")
-agent.add_edge("tools", "model") # Always run "model" after "tools"
-
-agent.add_conditional_edges(
-    "model", pending_tool_calls, {"tools": "tools", "done": END}
-)
-
-sql_agent = agent.compile(
-    checkpointer=MemorySaver(),
-)
+sql_agent = SqlAgent(model=model, db=db, tools=tools).get_agent()
